b727b988cccc8529a41a97f65b35fa85cb897fd3
Seth McCullough <seth@endevrs.dev>
2022-09-22 14:58:51 -0400

    Initialize react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+
+import PlausibleProvider from 'next-plausible';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppProps as NextAppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type AppProps<P = any> = NextAppProps & {
+  pageProps: P;
+} & Omit<NextAppProps<P>, 'pageProps'>;
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <PlausibleProvider domain="xbeach-ui.vercel.app">
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </PlausibleProvider>
+  );
 }
-
-export default MyApp;
