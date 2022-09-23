1bd4a995694c7c29dcf892048ea76bea72d08192
Seth McCullough <seth@endevrs.dev>
2022-09-23 07:47:59 -0400

    Restructure params api to categorize variables

--- pages/api/xbeach/params.ts
+++ pages/api/xbeach/params.ts
@@ -10,39 +10,66 @@ export default handler;
 
 async function readParams(req: NextApiRequest, res: NextApiResponse) {
   const response = await fetch(
-    'https://raw.githubusercontent.com/openearth/xbeach-docs/master/docs/tables/partable_all.tab',
+    'https://raw.githubusercontent.com/openearth/xbeach-docs/master/docs/xbeach_manual.rst',
   );
   const text = await response.text();
+
+  const tables: Array<string> = [];
+
+  for (let line of text.split('\n')) {
+    if (line.startsWith('.. include::')) {
+      tables.push(line.split('/')[1].replace('\r', ''));
+    }
+  }
 
   const params = {};
   let currentParam: string;
 
-  for (let line of text.split('\n')) {
-    line = line.trim();
+  await Promise.all(
+    tables.map(async (table) => {
+      const title = table.split('partable_')[1].replace('.tab', '');
+      params[title] = {};
 
-    if (line.match(/^[a-zA-Z]/)) {
-      currentParam = line;
-      params[currentParam] = {};
-    } else if (line.match(/^:/)) {
-      let [, key, value] = line.split(':');
-      key = key.trim();
-      value = value.trim() || '-';
-      if (key.match(/(advanced|silent|required)/i)) {
-        key.split(',').forEach((k) => {
-          params[currentParam][k] = true;
-        });
-      } else {
-        params[currentParam][key] = value;
+      const response = await fetch(
+        `https://raw.githubusercontent.com/openearth/xbeach-docs/master/docs/tables/${table}`,
+      );
+      const text = await response.text();
+
+      for (let line of text.split('\n')) {
+        line = line.trim();
+
+        if (line.match(/^[a-zA-Z]/)) {
+          currentParam = line;
+          params[title][currentParam] = {};
+        } else if (line.match(/^:/)) {
+          let [, key, value] = line.split(':');
+          key = key.trim();
+          value = value.trim() || '-';
+          if (key.match(/(advanced|silent|required)/i)) {
+            key.split(',').forEach((k) => {
+              params[title][currentParam][k] = true;
+            });
+          } else {
+            params[title][currentParam][key] = value;
+          }
+        } else {
+          continue;
+        }
       }
-    } else {
-      continue;
-    }
-  }
+    }),
+  );
 
-  const paramsArray = Object.keys(params).map((key) => ({
-    name: key,
-    ...params[key],
-  }));
+  const paramsArray = Object.keys(params).map((key) => {
+    return {
+      title: key,
+      params: Object.keys(params[key]).map((param) => {
+        return {
+          name: param,
+          ...params[key][param],
+        };
+      }),
+    };
+  });
 
   res.status(200).json({ data: paramsArray });
 }
