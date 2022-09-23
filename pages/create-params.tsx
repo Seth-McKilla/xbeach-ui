6c6d468221576180ea277a30ec8938c872357e4c
Seth McCullough <seth@endevrs.dev>
2022-09-23 07:31:53 -0400

    Add switch input to params page

--- pages/create-params.tsx
+++ pages/create-params.tsx
@@ -1,4 +1,4 @@
-import { Loader, InputNumber, InputText } from 'components';
+import { Loader, InputNumber, InputSwitch, InputText } from 'components';
 import { useReadXBeachParams } from 'hooks';
 
 import type { NextPage } from 'next';
@@ -12,7 +12,16 @@ const CreateParams: NextPage = () => {
     <div className="container max-w-6xl p-2 mx-auto">
       <div className="grid grid-cols-1 gap-4 p-4 mx-auto md:grid-cols-4 sm:grid-cols-3">
         {xBeachParams?.map((param: any, idx: number) => {
-          if (+param.default) {
+          if (param.range === '0 - 1') {
+            return (
+              <InputSwitch
+                key={`${idx}-${param.name}`}
+                name={param.name}
+                description={param.description}
+                defaultValue={!!+param.default}
+              />
+            );
+          } else if (+param.default) {
             return (
               <InputNumber
                 key={`${idx}-${param.name}`}
