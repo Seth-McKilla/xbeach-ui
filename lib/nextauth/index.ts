bd7cb3071c8f86378884640ccfaef343a4c36742
Seth McCullough <seth@endevrs.dev>
2022-12-11 14:16:55 -0500

    Switch to jwt session strategy and update db schema

---
+++ lib/nextauth/index.ts
@@ -0,0 +1,19 @@
+import EmailProvider from "next-auth/providers/email";
+import type { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
