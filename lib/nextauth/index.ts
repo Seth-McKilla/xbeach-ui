294548fc4987b87874321862a05b3f636906bf60
Seth McCullough <seth@endevrs.dev>
2022-12-12 08:11:25 -0500

    Remove prisma

--- lib/nextauth/index.ts
+++ lib/nextauth/index.ts
@@ -1,11 +1,7 @@
 import EmailProvider from "next-auth/providers/email";
 import type { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-import prisma from "lib/prisma";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
