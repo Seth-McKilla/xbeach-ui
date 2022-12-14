eff67c75d7de9fcf486cf8d0ca0215c85270c07d
Seth McCullough <seth@endevrs.dev>
2022-12-14 10:14:53 -0500

    Setup models app pages

---
+++ app/dashboard/projects/[id]/ListModels.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import LinkCard from "../../../components/LinkCard";
+
+type Props = {
+  models: Array<{
+    _id: string;
+    name: string;
+    description: string;
+  }>;
+};
+
+export default function ListModels({ models }: Props) {
+  return (
+    <div className="container grid grid-cols-1 gap-4 mx-auto mb-6 sm:grid-cols-3 lg:grid-cols-5">
+      {models?.map(({ _id, name, description }) => (
+        <LinkCard
+          key={_id}
+          href={`dashboard/models/${_id}`}
+          title={name}
+          description={description}
+        />
+      ))}
+    </div>
+  );
+}
--- app/dashboard/projects/[id]/page.tsx
+++ app/dashboard/projects/[id]/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import useSWR, { mutate } from "swr";
+import useSWR from "swr";
 
+import ListModels from "./ListModels";
 import { fetcher } from "lib/api/utils";
 
 export default function DashboardPage() {
   const pathname = usePathname();
   const [, , , id] = pathname.split("/");
   const { data: project } = useSWR(`/api/xbeach/projects/${id}`, fetcher);
-  console.log(project);
 
-  return <div>{id}</div>;
+  return (
+    <div className="mt-8">
+      <h1 className="text-2xl font-bold text-blue-800">{project?.name}</h1>
+      <div className="mt-4">
+        <ListModels models={project?.models} />
+      </div>
+    </div>
+  );
 }
