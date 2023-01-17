46471c8d2093789722e7ba53eef333f9f34e3b7f
Seth McCullough <seth@endevrs.dev>
2023-01-17 14:25:37 -0500

    Simplify middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,3 @@
-import { type NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
-export default async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const token = await getToken({ req });
-
-  if (pathname === "/signin" && !!token) {
-    return NextResponse.redirect(new URL("/app", req.url));
-  }
-
-  if (pathname.startsWith("/app") && !token) {
-    return NextResponse.redirect(new URL("/signin", req.url));
-  }
-
-  return NextResponse.next();
-}
+export const config = { matcher: "/app" };
